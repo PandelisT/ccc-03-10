14b92d53e88cb030d88a54e30552d21d9a42248a
PandelisT <pandeli@nerdypandy.com>
2020-11-06 11:38:08 +1100

    First commit

---
+++ database.py
@@ -0,0 +1,13 @@
+import psycopg2
+
+connection = psycopg2.connect(
+    database="library_api",
+    user="app",
+    password="app",
+    host="localhost"
+)
+
+cursor = connection.cursor()
+
+cursor.execute("create table if not exists books (id serial PRIMARY KEY, title varchar);")
+connection.commit()
