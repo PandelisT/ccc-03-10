e3bcacc272f4410659b8c7de4048313bc7d81506
PandelisT <pandeli@nerdypandy.com>
2020-11-12 12:28:28 +1100

    Added validation

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Flask application creation
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 app.config.from_object("default_settings.app_config")
 
@@ -15,8 +15,16 @@ db = init_db(app)
 from flask_marshmallow import Marshmallow
 ma = Marshmallow(app)
 
+from commands import db_commands
+app.register_blueprint(db_commands)
+
 #Controller registration
 from controllers import registerable_controllers
 for controller in registerable_controllers:
     app.register_blueprint(controller)
 
+# error handler for bad request (e.g. no title included in post request)
+from marshmallow.exceptions import ValidationError
+@app.errorhandler(ValidationError)
+def handle_bad_request(error):
+    return (jsonify(error.messages), 400)
--- schemas/BookSchema.py
+++ schemas/BookSchema.py
@@ -1,9 +1,13 @@
 from main import ma
 from models.Book import Book
-
+from marshmallow.validate import length
 class BookSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Book
+
+    # add validation
+    title = ma.String(required=True, validate=Length(min=1))
+
 
 book_schema = BookSchema()
 books_schema = BookSchema(many=True)
