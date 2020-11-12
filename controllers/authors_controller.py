004dd35e5e46f0e404c374877668dd5891e547ad
PandelisT <pandeli@nerdypandy.com>
2020-11-13 10:42:44 +1100

    Added User model and schema

--- controllers/authors_controller.py
+++
@@ -1,8 +0,0 @@
-from database import cursor, connection
-from flask import Blueprint, request, jsonify
-authors = Blueprint("authors", __name__, url_prefix="/authors")
-
-
-@authors.route("/", methods=["GET"])
-def authors_index():
-    return "authors"
