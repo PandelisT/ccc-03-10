d0cacfa04919dd66d8a47214f068480b119e3909
PandelisT <pandeli@nerdypandy.com>
2020-11-13 12:21:41 +1100

    Added login

--- schemas/UserSchema.py
+++ schemas/UserSchema.py
@@ -4,6 +4,7 @@ from marshmallow.validate import Length, Email
 class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = User
+        load_only = ["password"]
 
     # add validation for user input before it goes into the database
     email = ma.String(required=True, validate=[Length(min=4), Email()])
