a01ce039cfa1dbaa6b8dc20ea0f8225cc5b2543b
PandelisT <pandeli@nerdypandy.com>
2020-11-16 11:01:50 +1100

    Image upload to S3 working

--- models/Book.py
+++ models/Book.py
@@ -1,6 +1,6 @@
 # model = table 
-
 from main import db
+from models.BookImage import BookImage
 
 class Book(db.Model):
     __tablename__ = "books"
@@ -8,6 +8,7 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String())
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    book_image = db.relationship("BookImage", backref="book", uselist=False)
 
     def __repr__(self):
         return f"<Book f{self.title}>"
